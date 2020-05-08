bf56f73767087067516ceb21f40c16f891e341c1
hhimanshu <harit.himanshu@yahoo.in>
2020-05-08 16:37:47 -0700

    Move Setup implementation to App. Rename methods

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -10,4 +10,19 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static int doubleTheNumber(int number) {
+        // do it in module02
+        // todo: fix the implementation
+        // return -1;
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers) {
+        var sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
 }
