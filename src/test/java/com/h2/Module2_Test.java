893ece15d4fda4ba0f83d9211399f696f550f55d
hhimanshu <harit.himanshu@yahoo.in>
2020-05-12 17:36:33 -0700

    Added 2 more tests for Module 02

--- src/test/java/com/h2/Module2_Test.java
+++ src/test/java/com/h2/Module2_Test.java
@@ -1,24 +1,69 @@
 package com.h2;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.function.Try;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.platform.commons.util.ReflectionUtils.*;
 
 public class Module2_Test {
-    private final String classToFind = "com.h2.App";
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
 
-    public Optional<Class<?>> getAppClass() {
-        Try<Class<?>> aClass = tryToLoadClass(classToFind);
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @BeforeEach
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @AfterEach
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    private static Optional<Class<?>> getClass(final String className) {
+        Try<Class<?>> aClass = tryToLoadClass(className);
         return aClass.toOptional();
     }
+
+    public Optional<Class<?>> getAppClass() {
+        final String classToFind = "com.h2.App";
+        return getClass(classToFind);
+    }
+
+    public Optional<Class<?>> getBestLoanRatesClass() {
+        final String classToFind = "com.h2.BestLoanRates";
+        return getClass(classToFind);
+    }
+
 
     @Test
     public void m02_01_testDoubleTheNumber() {
@@ -29,6 +74,7 @@ public class Module2_Test {
 
     @Test
     public void m02_02_assertPrivateMethodExistence() {
+
         final String methodName = "add";
         final Optional<Class<?>> maybeClass = getAppClass();
         assertTrue(maybeClass.isPresent());
@@ -64,6 +110,39 @@ public class Module2_Test {
         @SuppressWarnings("PrimitiveArrayArgumentToVarargsMethod")
         int sum123 = (int) method.invoke(aClass, new int[]{1, 2, 3});
         assertEquals(6, sum123, "1 + 2 + 3 should be 6, got " + sum123);
+    }
+
+    @Test
+    public void m02_04_assertBestLoanRatesExistence() {
+        final String classToFind = "com.h2.BestLoanRates";
+        final Optional<Class<?>> maybeClass = getBestLoanRatesClass();
+        assertTrue(maybeClass.isPresent(), classToFind + " should be present");
+
+        Class<?> aClass = maybeClass.get();
+        assertEquals(classToFind, aClass.getCanonicalName());
+        assertTrue(isPublic(aClass));
+    }
+
+    @Test
+    public void m2_05_assertMainMethodExistence() {
+        final String main = "main";
+        final Optional<Class<?>> maybeClass = getBestLoanRatesClass();
+        assertTrue(maybeClass.isPresent());
+        Class<?> c = maybeClass.get();
+        List<Method> methods = Arrays.stream(c.getDeclaredMethods())
+                .filter(m -> m.getName().equals(main))
+                .collect(Collectors.toList());
+
+        assertEquals(1, methods.size(), main + " must be defined as a method in " + c.getCanonicalName());
+
+        final Method method = methods.get(0);
+        assertEquals(void.class, method.getReturnType(), main + " must return 'void' as the return type");
+        assertTrue(isStatic(method), main + " must be a static method");
+        assertTrue(isPublic(method), main + " must be a public method");
+
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        assertEquals(1, parameterTypes.length, main +  " should accept 1 parameter");
+        assertEquals(String[].class, parameterTypes[0], main + " parameter type should be of 'String[]' type");
     }
 
 }
