458d6636c3bc91e033caf10e32a6a5f1048d1a7a
snychka <stefannychka@gmail.com>
2020-05-18 07:16:47 -0600

    partially done module2

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,28 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+
+        return number*2;
+    }
+
+    /*
+    Next, create a new method called `add()`.
+    This method should be `private`, `static`,
+    and should have a return type of `int`.
+    The method should take one parameter as its input.
+    The parameter name should be `numbers`, and should be of type `int[]`.
+
+     */
+
+    private static int add(int[] numbers) {
+        /*For the implementation of `add()` method,
+        create a variable called `sum`, and initialize its value to `0`.
+        Then, using a `for` loop over `numbers`,
+        add every item in the `numbers` array to the `sum` variable. Finally, return `sum`.*/
+        var sum = 0;
+        for (var n: numbers) {
+            sum += n;
+        }
+        return sum;
     }
 }
