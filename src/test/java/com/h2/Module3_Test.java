f3b6719b14ce64d64abae2d07c63d531ca609e8e
hhimanshu <harit.himanshu@yahoo.in>
2020-05-18 16:36:09 -0700

    Added m3_02_testExistenceOfPrivateFields

--- src/test/java/com/h2/Module3_Test.java
+++ src/test/java/com/h2/Module3_Test.java
@@ -9,10 +9,15 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.platform.commons.util.ReflectionUtils.isPrivate;
 import static org.junit.platform.commons.util.ReflectionUtils.tryToLoadClass;
 
 public class Module3_Test {
@@ -54,8 +59,25 @@ public class Module3_Test {
     }
 
     @Test
-    public void m02_01_testSavingsCalculatorExists() {
+    public void m03_01_testSavingsCalculatorExists() {
         Optional<Class<?>> maybeClass = getSavingsClass();
-        assertTrue(maybeClass.isPresent(), classToFind + " must exist");
+        assertTrue(maybeClass.isPresent(), classToFind + " must be created.");
+    }
+
+    @Test
+    public void m3_02_testExistenceOfPrivateFields() {
+        final Optional<Class<?>> maybeSavingsCalculator = getSavingsClass();
+        assertTrue(maybeSavingsCalculator.isPresent(), classToFind + " must exist");
+        final Class<?> savingsCalculator = maybeSavingsCalculator.get();
+        final Set<String> fieldNames = new HashSet<>(Arrays.asList("credits", "debits"));
+
+        final Field[] declaredFields = savingsCalculator.getDeclaredFields();
+        assertEquals(2, declaredFields.length, "2 fields (credits and debits) should be available in " + classToFind);
+
+        for (final Field field : declaredFields) {
+            assertTrue(isPrivate(field), field.getName() + " should be declared private");
+            assertEquals(float[].class, field.getType(), field.getName() + " should be of type 'float[]'");
+            assertTrue(fieldNames.contains(field.getName()), field.getName() + " is not a valid name. It should be either 'credits' or 'debits'");
+        }
     }
 }
