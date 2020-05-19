4e3f0a7db072175757c89d93ea917b834a27218c
hhimanshu <harit.himanshu@yahoo.in>
2020-05-18 17:35:23 -0700

    Added m3_06_testSumOfCreditsWorksCorrectly

--- src/test/java/com/h2/Module3_Test.java
+++ src/test/java/com/h2/Module3_Test.java
@@ -128,7 +128,7 @@ public class Module3_Test {
         final String methodName = "sumOfCredits";
 
         final Optional<Class<?>> maybeSavingsCalculator = getSavingsClass();
-        assertTrue(maybeSavingsCalculator.isPresent());
+        assertTrue(maybeSavingsCalculator.isPresent(), classToFind + " must exist");
         final Class<?> savingsCalculator = maybeSavingsCalculator.get();
 
         final Method[] methods = savingsCalculator.getDeclaredMethods();
@@ -141,4 +141,27 @@ public class Module3_Test {
         assertEquals(float.class, sumOfCredits.getReturnType(), methodName + " method must return a value of type 'float'");
     }
 
+    @Test
+    public void m3_06_testSumOfCreditsWorksCorrectly() throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        final Optional<Class<?>> maybeSavingsCalculator = getSavingsClass();
+        assertTrue(maybeSavingsCalculator.isPresent(), classToFind + " must exist");
+        final Class<?> savingsCalculator = maybeSavingsCalculator.get();
+        final Constructor<?>[] constructors = savingsCalculator.getDeclaredConstructors();
+
+        assertEquals(1, constructors.length, classToFind + " should have 1 constructor");
+
+        float[] credits = new float[]{10.0f, 20.0f};
+        float[] debits = new float[]{5.0f};
+        final Constructor<?> constructor = constructors[0];
+        Object instance = constructor.newInstance(credits, debits);
+
+        final String methodName = "sumOfCredits";
+        final Method[] methods = savingsCalculator.getDeclaredMethods();
+        final List<Method> filteredMethod = Arrays.stream(methods).filter(method -> method.getName().equals(methodName)).collect(Collectors.toList());
+        assertEquals(1, filteredMethod.size(), classToFind + " should contain a method called '" + methodName + "'");
+        Method method = filteredMethod.get(0);
+        final float result = (float) invokeMethod(method, instance);
+
+        assertEquals(30.0f, result, methodName + " is not returning sum of credits.");
+    }
 }
